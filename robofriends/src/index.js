c272e701b4182067f4e4cce8069618efe3ff648a
Shea <galley.shea@gmail.com>
2021-11-17 10:45:03 -0500

    223 | First React Component

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom'; //we specify React-DOM here because React can be used in many other types of software dev.  this one specifies for web & browsers
+import './index.css';     //  ./ indicates the same directory.  so the same folder as index.js
+import App from './App';  // .js file is assumed as there is no other .
+import reportWebVitals from './reportWebVitals';  //advanced - don't worry.  testing?
+
+import Hello from './Hello';
+import 'tachyons';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* below we create a prop (property) called greeting which is a string*/}
+    <Hello greeting={'Hello, React Ninja'}/> 
+    <App />                 
   </React.StrictMode>,
   document.getElementById('root')
 );
