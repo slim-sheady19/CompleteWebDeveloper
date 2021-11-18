6c807fc6b1f12f3e2a3cd66c93ec99754a379918
Shea <galley.shea@gmail.com>
2021-11-18 10:40:02 -0500

    231 | Ract App 4: componentDidMount() and fetch()

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -14,12 +14,21 @@ class App extends React.Component { //App class declared here (child of React.Co
         }
     }
 
+    //this function is similar to Start or Awake in Unity.  it is called after the component is spawned
+    componentDidMount() {
+        //https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({robots: users}));
+    }
+
     //this function updates the state of the searchbox
     onSearchChange = (event) => { //we use the = (event) => in order to keep the this keyword used below pointing to the state object
         console.log(event.target.value); //print the actual input rather than the signal being sent with .target.value
         this.setState({ searchfield: event.target.value }) //need to use this.setState React method when we want to affect state props 
     }
 
+    //render function is always called when components have mounted and whenever a change in state is detected
     render () {
         //create array of robots that are filtered with the search field text and changed dynamically.
         //it is created inside scope of render function to be accessed in return below
